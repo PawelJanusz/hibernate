e4fe4d3b51ead2a54a090a3d4bb59029337f095d
Pawel Janusz <marek80k@wp.pl>
2020-05-17 11:43:48 +0200

    hibernate1

---
+++ src/main/java/com/sda/javawro27/hibernate/HibernateUtil.java
@@ -0,0 +1,35 @@
+package com.sda.javawro27.hibernate;
+
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+// ta klasa załaduje konfigurację bazodanową
+public class HibernateUtil {
+
+    private static final SessionFactory ourSessionFactory;
+
+    // blok statyczny - fragment kodu który wykonuje się 1 raz przy starcie klasy
+    static {
+        try {
+            System.out.println("Konfiguruję hibernate");
+
+            // ta klasa odwołuje sie do pliku konfiguracyjnego hibernate w formacie podanym w recources
+            Configuration configuration = new Configuration();
+
+            configuration.configure("/hibernate.cfg.xml");
+
+            ourSessionFactory = configuration.buildSessionFactory();
+        } catch (HibernateException he){
+            System.out.println(he.getMessage());
+            System.exit(376); // liczba która jest kodem błedu - jeśli zobaczymy liczbę znaczy że jest to bład hibernate
+            throw he;
+        }
+    }
+
+    // wygenerowany getter
+
+    public static SessionFactory getOurSessionFactory() {
+        return ourSessionFactory;
+    }
+}
